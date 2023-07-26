4f9edde2a1353940d91b1d6954f6224a1133d4ca
ali jarar <alijarar5@gmail.com>
2023-07-26 14:46:49 +0400

    nav bar

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+const UserMenu = () => {
+  return (
+      <div className="relative">
+          <div className="flex flex-row items-center gap-3">
+              <div
+                  className="hidden px-4 py-3 text-sm font-semibold transition rounded-full cursor-pointer md:block hover:bg-neutral-100"
+                  onClick={()=>{}}
+              >
+                  Book your home
+
+              </div>
+          </div>
+    </div>
+  )
+}
+
+export default UserMenu
