6b5fd97abf5e3b2f72cd8c9de9106a328daf8baf
ali jarar <alijarar5@gmail.com>
2023-11-23 14:42:10 -0500

    intial commit user Menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,19 +1,37 @@
-"use client";
+'use client'
+import React, { useState } from 'react';
 
-const UserMenu = () => {
-  return (
-      <div className="relative">
-          <div className="flex flex-row items-center gap-3">
-              <div
-                  className="hidden px-4 py-3 text-sm font-semibold transition rounded-full cursor-pointer md:block hover:bg-neutral-100"
-                  onClick={()=>{}}
-              >
-                  Book your home
-
-              </div>
-          </div>
-    </div>
-  )
+interface Coworker {
+  first_name: string;
+  last_name: string;
+  inOffice: boolean;
 }
 
-export default UserMenu
+const CoworkerList: React.FC = () => {
+  const [coworkers, setCoworkers] = useState<Coworker[]>([
+    { first_name: 'Max', last_name: 'Mustermann', inOffice: false },
+    { first_name: 'Vladimir', last_name: 'Leles', inOffice: false },
+    { first_name: 'Tobias', last_name: 'Anhalt', inOffice: false },
+  ]);
+
+  const toggleInOfficeStatus = (index: number) => {
+    setCoworkers((prevCoworkers) => {
+      const updatedCoworkers = [...prevCoworkers];
+      updatedCoworkers[index].inOffice = !updatedCoworkers[index].inOffice;
+      return updatedCoworkers;
+    });
+  };
+
+  return (
+    <div>
+      {coworkers.map((coworker, index) => (
+        <div key={index}>
+          <p>Name: {coworker.first_name} {coworker.last_name}</p>
+          <button onClick={() => toggleInOfficeStatus(index)}>{coworker.inOffice ? 'In office' : 'Out of office'}</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CoworkerList;
