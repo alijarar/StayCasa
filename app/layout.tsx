c52ca625134bde74391c8245822b06f7e23378dd
ali jarar <alijarar5@gmail.com>
2023-12-11 16:09:04 -0500

    added button and modal inital

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
 
 const inter = Inter({ subsets: ['latin'] })
 const font =Nunito({
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal actionLabel='myLabel' title='Hello World' isOpen={true} />
         <Navbar/>
         {children}
       </body>
