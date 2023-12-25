81fef048e0e67ac89350e1f58cc92235500e67de
ali jarar <alijarar5@gmail.com>
2023-12-24 20:39:14 -0500

    Listing creation step 5 (cloudinary Image,Description and Price, Listing creation POST route) completed

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,12 +5,15 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/inputs";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 enum STEPS {
   CATEGORY = 0,
@@ -22,6 +25,7 @@ enum STEPS {
 }
 
 const RentModal = () => {
+  const router = useRouter()
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
@@ -73,6 +77,29 @@ const RentModal = () => {
 
   const onNext = () => {
     setStep((value) => value + 1);
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext()
+    }
+
+    setIsLoading(true);
+
+    axios.post('/api/listings', data)
+      .then(() => {
+        toast.success("Listing Created");
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose()
+        
+      }).catch(() => {
+      toast.error("Something went Wrong")
+      }).finally(() => {
+        setIsLoading(false);
+    })
+    
   }
 
   const actionLabel = useMemo(() => {
@@ -228,6 +255,7 @@ const RentModal = () => {
           disabled={isLoading}
           register={register}
           errors={errors}
+          required
         />
       </div>
     )
@@ -238,7 +266,7 @@ const RentModal = () => {
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
